6a75f7f920e29d8c9a02b55690367be85561d84a
Roman Njoroge <roman.njoroge@njuguna.com>
2025-12-16 20:55:53 +0300

    feat: Getting number of paper rolls that are reachable

--- day4/puzzle.py
+++ day4/puzzle.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def g(x_graph: int, y_graph: int) -> int:
     """
     This function checks whether the item at coordinate (x_graph ,y_graph) is a roll of paper
@@ -21,7 +22,7 @@ def r(x_graph: int, y_graph: int) -> int:
     This function returns how many paper roles are around the item around (x_graph, y_graph)
     """
     
-    return (g(x_graph=x_graph - 1, y_graph=y_graph + 1) + # 
+    return (g(x_graph=x_graph - 1, y_graph=y_graph + 1) +  
             g(x_graph=x_graph, y_graph=y_graph + 1) +
             g(x_graph=x_graph + 1, y_graph=y_graph + 1) +
             g(x_graph=x_graph - 1, y_graph=y_graph) +
@@ -29,6 +30,36 @@ def r(x_graph: int, y_graph: int) -> int:
             g(x_graph=x_graph + 1, y_graph=y_graph - 1) +
             g(x_graph=x_graph, y_graph=y_graph - 1) +
             g(x_graph=x_graph - 1, y_graph=y_graph - 1))
+    
+def a(x_graph: int, y_graph: int) -> int:
+    """
+    This function returns if the paper roll at position (x_graph, y_graph)
+    is accessible by a forklift.
+    
+    A paper roll is accessible if there are less than 4 paper rolls around it.
+    
+    1 is returned if it is accessible otherwise 0 is returned
+    
+    If (x_graph, y_graph) is not a roll, ignore and return 0
+    """
+    is_position_role = g(x_graph=x_graph, y_graph=y_graph)
+    if is_position_role == 0:
+        return 0
+    
+    rolls_around_position = r(x_graph=x_graph, y_graph=y_graph)
+    if rolls_around_position < 4:
+        return 1
+    else:
+        return 0
+    
+def part_1():
+    sum_reachable = 0
+    for y_comp, row in enumerate(graph):
+        y_graph = height - 1 - y_comp
+        for x_graph, _ in enumerate(row):
+            sum_reachable += a(x_graph=x_graph, y_graph=y_graph)
+            
+    print(f"The number of rolls that are reachable are {sum_reachable}")
     
 graph = [
     "..@@.@@@@.",
@@ -42,13 +73,18 @@ graph = [
     ".@@@@@@@@.",
     "@.@.@@@.@."
 ]
+# raw_input = pd.read_csv("day4/input.txt", header=None)
+# graph = raw_input[0]
 width = len(graph[0])
 height = len(graph)
 
-x_graph = 4
+x_graph = 5
 y_graph = 4
 
 print(f"Width is {width} and height is {height}")
 print(f"g of Position at ({x_graph}, {y_graph}) is {g(x_graph=x_graph, y_graph=y_graph)}")
-print(f"The number of paper rolls around position ({x_graph}, {y_graph}) is {r(x_graph=x_graph, y_graph=y_graph)}")
+# print(f"The number of paper rolls around position ({x_graph}, {y_graph}) is {r(x_graph=x_graph, y_graph=y_graph)}")
+# print(f"Is position ({x_graph, y_graph}) accessible by forklift {a(x_graph=x_graph, y_graph=y_graph)}")
+part_1()
+
 
