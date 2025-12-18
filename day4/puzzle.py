054b78ceb0d99ab1e8d0f43d48913acbad2cff36
Roman Njoroge <roman.njoroge@njuguna.com>
2025-12-18 21:56:37 +0300

    feat: Further stuff

--- day4/puzzle.py
+++ day4/puzzle.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import time
+
 def g(x_graph: int, y_graph: int) -> int:
     """
     This function checks whether the item at coordinate (x_graph ,y_graph) is a roll of paper
@@ -73,22 +75,58 @@ def markRemoved(x_graph: int, y_graph: int):
         y_list = height - 1 - y_graph
         graph[y_list] = graph[y_list][:x_list] + "X" + graph[y_list][x_list + 1:]
     
-
+removable_rolls = []
+sum_removed = 0
+def part_2():
+    """
+    Try removing as many rolls of paper as possible
+    """
+    global sum_removed
     
-graph = [
-    "..@@.@@@@.",
-    "@@@.@.@.@@",
-    "@@@@@.@.@@",
-    "@.@@@@..@.",
-    "@@.@@@@.@@",
-    ".@@@@@@@.@",
-    ".@.@.@.@@@",
-    "@.@@@.@@@@",
-    ".@@@@@@@@.",
-    "@.@.@@@.@."
-]
-# raw_input = pd.read_csv("day4/input.txt", header=None)
-# graph = raw_input[0]
+    print(f"\n INITIAL GRAPH")
+    # print_graph()
+    rolls_removed = 0
+    
+    while True:
+        # Get a list of items that can be removed while counting them
+        for y_comp, row in enumerate(graph):
+            y_graph = height - 1 - y_comp
+            for x_graph, _ in enumerate(row):
+                if a(x_graph=x_graph, y_graph=y_graph):
+                    rolls_removed += 1
+                    sum_removed += 1
+                    removable_rolls.append((x_graph, y_graph))
+                    
+                
+        # Remove items that can be removed
+        for removable in removable_rolls:
+            markRemoved(x_graph=removable[0], y_graph=removable[1])
+        
+        print(f"The number of rolls that were removed this session {rolls_removed}")
+        # print_graph()
+        
+        # If rolls_removed == 0 stop
+        if rolls_removed == 0:
+            break
+        else:
+            rolls_removed = 0
+            
+    print(f"Total number removed {sum_removed}")
+
+# graph = [
+#     "..@@.@@@@.",
+#     "@@@.@.@.@@",
+#     "@@@@@.@.@@",
+#     "@.@@@@..@.",
+#     "@@.@@@@.@@",
+#     ".@@@@@@@.@",
+#     ".@.@.@.@@@",
+#     "@.@@@.@@@@",
+#     ".@@@@@@@@.",
+#     "@.@.@@@.@."
+# ]
+raw_input = pd.read_csv("day4/input.txt", header=None)
+graph = raw_input[0]
 width = len(graph[0])
 height = len(graph)
 
@@ -100,6 +138,12 @@ print(f"g of Position at ({x_graph}, {y_graph}) is {g(x_graph=x_graph, y_graph=y
 # print(f"The number of paper rolls around position ({x_graph}, {y_graph}) is {r(x_graph=x_graph, y_graph=y_graph)}")
 # print(f"Is position ({x_graph, y_graph}) accessible by forklift {a(x_graph=x_graph, y_graph=y_graph)}")
 # print_graph()
-part_2()
+
+
+if __name__ == "__main__":
+    start_time = time.perf_counter()
+    part_2()
+    end_time = time.perf_counter()
+    print(f"Elapsed time is {(end_time - start_time):.4f}")
 
 
