bbe4527213dda9009e6538ce704769bc9d7c31af
Roman Njoroge <roman.njoroge@njuguna.com>
2025-12-21 12:53:57 +0300

    feat: Function for getting possible column space in string

---
+++ day6/puzzle.py
@@ -0,0 +1,99 @@
+"""
+Try seeing if I can get a stream of each line in input file
+
+For each line, get the next number and end buffer. Get number by seeing strings whos ord [0, 9]
+
+Add up numbers from each line
+
+When reach end of line of any line stop
+"""
+import math
+import re
+
+def part_1():
+    result = 0
+    # Get stream for each line in file
+    with open("day6/input.txt", "r") as file:
+        iterators = [line.split() for line in file]
+        
+        for args in zip(*iterators):
+            operation = args[-1]
+            numbers = [int(x) for x in args[0:-1]]
+            if operation == "+":
+                result += sum(numbers)
+            else:
+                result += math.prod(numbers)
+            
+    print(f"Result is {result}")
+    
+# part_1()
+
+def cephalapod_math(item: list[str]) -> int:
+    print(f"Doing cephalapod math for {item}")
+    raw_numbers = item[0:-1]
+    operation = item[-1]
+    intepreted_nums = []
+    place = 1
+    
+    while True:
+        num_has_place = False
+        num_to_add = ""
+        for num in raw_numbers:
+            if len(num) >= place:
+                num_has_place = True
+                num_to_add += num[place - 1]
+                
+        if (num_has_place == False):
+            break
+        else:
+            intepreted_nums.append(int(num_to_add))
+            place += 1
+            
+    print(f"Interpreted nums are {intepreted_nums}")
+        
+    if operation == "+":
+        return sum(intepreted_nums)
+    else:
+        return math.prod(intepreted_nums)
+    
+def get_column_space_position(x: str, start: int) -> int:
+    """
+    This function gets possible position of column space in string
+    
+    Column space is point in string that is empty space and comes after a number
+    """
+    number_found = False
+    column_space = start
+    
+    for char in x[start:]:
+        print(f"Looking at {char}, {char.isspace()}")
+        if number_found:
+            if char.isspace():
+                break
+        else:
+            if char != "":
+                number_found = True
+        column_space += 1
+                
+    return column_space
+                
+                
+    
+def part_2():
+    result = 0
+    # Get stream for each line in file
+    with open("day6/input.txt", "r") as file:
+        lines = [line for line in file]
+        operations = [line.strip() for line in lines[-1]]
+        raw_numbers = lines[0:-1]
+        
+    #     for args in zip(*iterators):
+    #         print(f"Answer for operation is {cephalapod_math(args)}")
+    #         result += cephalapod_math(args)
+            
+    # print(f"Result is {result}")
+    
+# part_2()
+start = 0
+test_string = "123 328  51 64"
+print(f"Next column space if starting at {start} for {test_string} is {get_column_space_position(test_string, start=start)}")                
