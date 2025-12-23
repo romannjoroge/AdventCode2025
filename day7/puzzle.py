24a53301d65471d2b9af949239b1b93d6acf8788
Roman Njoroge <roman.njoroge@njuguna.com>
2025-12-23 23:32:51 +0300

    feat: Getting new beams correctly

--- day7/puzzle.py
+++ day7/puzzle.py
@@ -33,8 +33,26 @@ def part_1():
                 beams.add(x_graph)
                 break
             
-        print(f"Found beams {beams}")
-        
+        print(f"Found beams {beams}")        
         # Find colliding splinters for each beam
-        
+        for y_graph in range(tachyon_manifold_height - 2, -1, -1):
+            # For each beam check if there is a splinter at corresponding y and x
+            beams_iter = list(beams)
+            for beam in beams_iter:
+                if get_item_at_coordinate(x_graph=beam, y_graph=y_graph) == "^":
+                    # Remove old beam and add collission
+                    beams.remove(beam)
+                    num_times_splinter_in_path += 1
+                    
+                    # Add 2 new beams if within range
+                    left_beam = beam - 1
+                    right_beam = beam + 1
+                    
+                    if 0 <= left_beam:
+                        beams.add(left_beam)
+                    if right_beam <= tachyon_manifold_width:
+                        beams.add(right_beam)
+                        
+                    print(f"New beams {beams} at y {y_graph} split at {beam}")
+
 part_1()
