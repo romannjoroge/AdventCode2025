f052de86aac46f38347fb4df218313c42481792e
Roman Njoroge <roman.njoroge@njuguna.com>
2025-12-23 23:14:51 +0300

    feat: Function for getting item at coordinate

--- day7/puzzle.py
+++ day7/puzzle.py
@@ -1,11 +1,35 @@
+tachyon_manifold = []
+tachyon_manifold_width = 0
+tachyon_manifold_height = 0
+
+def get_item_at_coordinate(x_graph: int, y_graph: int) -> str:
+    """
+    Get's the item at a coordinate. Item can either be S, . or ^
+    """
+    x_list = x_graph
+    y_list = tachyon_manifold_height - 1 - y_graph
+    return tachyon_manifold[y_list][x_list]
+
 def part_1():
     """
     Counts the number of times splinter is in path of beam
     """
+    global tachyon_manifold_width
+    global tachyon_manifold
+    global tachyon_manifold_height
+    
     num_times_splinter_in_path = 0
+    beams = set()
     
     with open("day7/input.txt", 'r') as file:
         tachyon_manifold = [line.strip() for line in file]
-        print(tachyon_manifold)
+        tachyon_manifold_height = len(tachyon_manifold)
+        tachyon_manifold_width = len(tachyon_manifold[0])
+        
+        # Find starting coordinate
+        
+        # Find colliding splinters for each beam
         
 part_1()
+
+print(get_item_at_coordinate(7, 13))
