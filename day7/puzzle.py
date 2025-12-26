ca1510025c64276f08998c050331a1a220f700a4
Roman Njoroge <roman.njoroge@njuguna.com>
2025-12-26 11:35:29 +0300

    feat: Getting root node

--- day7/puzzle.py
+++ day7/puzzle.py
@@ -60,7 +60,8 @@ def part_1():
         print(f"Has split {num_times_splinter_in_path} times")
         
 class Node:
-    def __init__(self, x_graph: int):
+    def __init__(self, x_graph: int, y_graph: int):
+        self.y_graph = y_graph
         self.x_graph = x_graph
         self.left = None
         self.right = None
@@ -70,79 +71,92 @@ class Node:
         
     def assign_right(self, right: 'Node'):
         self.right = right
+        
+    def __repr__(self):
+        return f"Node with coordinate ({self.x_graph}, {self.y_graph})"
+    
+    def __eq__(self, other):
+        if other == None:
+            return False
+        elif not isinstance(other, Node):
+            return False
+        return self.y_graph == other.y_graph and self.x_graph == other.x_graph
+    
+    def __hash__(self):
+        return hash((self.x_graph, self.y_graph))
+    
+class Beam:
+    def __init__(self, x_graph: int, parent_node: Node):
+        self.x_graph = x_graph
+        self.parent_node = parent_node
+        
+    def __repr__(self):
+        return f"Beam: x = {self.x_graph} from {self.parent_node}"
+    
+    def __eq__(self, other):
+        if other == None:
+            return False
+        elif not isinstance(other, Beam):
+            return False
+        return self.x_graph == other.x_graph and self.parent_node == other.parent_node
+    
+    def __hash__(self):
+        return hash((self.x_graph, self.parent_node))
 
 def get_num_paths_from_node(node: Node) -> int:
     """
     Function for recursively getting number of paths in node by recursively
     adding up number of paths from children
     """
+    print(f"Getting number of points for node ({node.x_graph}, {node.y_graph})")
     sum = 0
     
     if node.left == None:
         sum += 1
+        print(f"Node has no left child adding 1 to sum {sum}")
     else:
         sum += get_num_paths_from_node(node.left)
+        print(f"Node had left child {node.left} making new sum {sum}")
         
     if node.right == None:
         sum += 1
+        print(f"Node had no right child, adding 1 to make sum {sum}")
     else:
         sum += get_num_paths_from_node(node.right)
+        print(f"Node had right child {node.right} adding its sum to get {sum}")
         
     return sum
     
-
-        
 def part_2():
-    """
-    Counts the number of paths that the particle could have used
-    """
     global tachyon_manifold_width
     global tachyon_manifold
     global tachyon_manifold_height
     
-    beams = set[Node]()
-    
+    # Get tree with possible paths
+    root_node = None
+    first_beam_x = None
+    # Import data
     with open("day7/input.txt", 'r') as file:
         tachyon_manifold = [line.strip() for line in file]
         tachyon_manifold_height = len(tachyon_manifold)
         tachyon_manifold_width = len(tachyon_manifold[0])
         
-        root_node = None
-        
-        # Find starting coordinate assuming it's at y = height
+        # Find coordinate of first beam
         for x_graph in range(tachyon_manifold_width):
             if get_item_at_coordinate(x_graph=x_graph, y_graph=tachyon_manifold_height - 1) == "S":
                 # Insert a beam
-                root_node = Node(x_graph=x_graph)
-                beams.add(root_node)
+                first_beam_x = x_graph
                 break
-            
-        # print(f"Found beams {beams}")        
-        # Find colliding splinters for each beam
-        for y_graph in range(tachyon_manifold_height - 2, -1, -1):
-            # For each beam check if there is a splinter at corresponding y and x
-            beams_iter = list(beams)
-            for beam in beams_iter:
-                if get_item_at_coordinate(x_graph=beam.x_graph, y_graph=y_graph) == "^":
-                    # Remove old beam and add collission
-                    beams.remove(beam)
-                   
-                    # Add 2 new beams if within range
-                    left_beam = beam.x_graph - 1
-                    right_beam = beam.x_graph + 1
-                    
-                    if 0 <= left_beam:
-                        left_child = Node(left_beam)
-                        beam.assign_left(left_child)
-                        beams.add(left_child)
-                    if right_beam <= tachyon_manifold_width:
-                        right_child = Node(right_beam)
-                        beam.assign_right(right_child)
-                        beams.add(right_child)
-                        
-                    # print(f"New beams {beams} at y {y_graph} split at {beam}")
         
-        print("Processing nodes")
+        # Find root node
+        for y_graph in range(tachyon_manifold_height - 2, -1, -1):
+            if get_item_at_coordinate(x_graph=first_beam_x, y_graph=y_graph) == "^":
+                root_node = Node(x_graph=first_beam_x, y_graph=y_graph)
+                break
+        
+    
+    print(f"Root node is {root_node} and x coordinate of first beam is {first_beam_x}")
+    # Calculate all possible paths
                     
         
 
@@ -152,12 +166,4 @@ def part_2():
 # elapsed_time = end_time - start_time
 # print(f"My code took {elapsed_time:.4f} seconds")
 
-# part_2()
-test_node = Node(1)
-test_right_child_node = Node(2)
-test_node.assign_right(test_right_child_node)
-test_left_child_node = Node(3)
-test_node.assign_left(test_left_child_node)
-test_right_grandchild_node = Node(4)
-test_right_child_node.assign_right(test_right_grandchild_node)
-print(f"Sum of paths from test node is {get_num_paths_from_node(test_node)}")
+part_2()
