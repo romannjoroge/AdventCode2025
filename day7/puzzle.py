495c044b2adba3d794692e95423d471e3724dc5c
Roman Njoroge <roman.njoroge@njuguna.com>
2025-12-26 21:01:46 +0300

    feat: Code for getting lookup table for test input is working"

--- day7/puzzle.py
+++ day7/puzzle.py
@@ -3,7 +3,7 @@ tachyon_manifold_width = 0
 tachyon_manifold_height = 0
 
 import time
-from collections import deque
+from collections import deque, defaultdict
 
 def get_item_at_coordinate(x_graph: int, y_graph: int) -> str:
     """
@@ -107,7 +107,7 @@ class Beam:
     def __hash__(self):
         return hash((self.parent, self.x))
     
-node_index = {}
+node_index = defaultdict(list)
 
 def part_2():
     """
@@ -138,16 +138,45 @@ def part_2():
         # Find root node
         root_splinter = None
         for y_graph in range(tachyon_manifold_height - 2, -1, -1):
-            if get_item_at_coordinate(x_graph=first_beam_x, y_graph=y_graph) == "^":
-                root_splinter = Splinter(x_graph=first_beam_x, y_graph=y_graph)
-                left_beam = Beam(parent_splinter=root_splinter, x=first_beam_x - 1)
-                right_beam = Beam(parent_splinter=root_splinter, x=first_beam_x + 1)
-                beams.add(left_beam)
-                beams.add(right_beam)
-                break
+            if len(beams) == 0:
+                if get_item_at_coordinate(x_graph=first_beam_x, y_graph=y_graph) == "^":
+                    # print(f"No beams so far looking for root splinter at y = {y_graph}")
+                    root_splinter = Splinter(x_graph=first_beam_x, y_graph=y_graph)
+                    left_beam = Beam(parent_splinter=root_splinter, x=first_beam_x - 1)
+                    right_beam = Beam(parent_splinter=root_splinter, x=first_beam_x + 1)
+                    beams.add(left_beam)
+                    beams.add(right_beam)
+            else:
+                beams_iter = list(beams)
+                # print(f"More than one beam {beams_iter} at y = {y_graph}")
+                for beam in beams_iter:
+                    # If there is a splinter at y level remove beam from set
+                    if get_item_at_coordinate(x_graph=beam.x, y_graph=y_graph) == "^":
+                        beams.remove(beam)
+                        new_splinter = Splinter(x_graph=beam.x, y_graph=y_graph)
+                        # print(f"New splinter found {new_splinter}")
+                    
+                        # Mark that parent splinter can reach this current splinter
+                        node_index[beam.parent.to_id()].append(new_splinter)
+                        
+                        # Insert 2 beams coming from new splinter
+                        left_beam_x = new_splinter.x - 1
+                        right_beam_x = new_splinter.x + 1
+                        
+                        if 0 <= left_beam_x:
+                            left_beam = Beam(parent_splinter=new_splinter, x=left_beam_x)
+                            # print(f"Adding left beam from splinter")
+                            beams.add(left_beam)
+                        if right_beam_x <= tachyon_manifold_width:
+                            right_beam = Beam(parent_splinter=new_splinter, x = right_beam_x)
+                            # print(f"Adding right child from splinter")
+                            beams.add(right_beam)
         
         # Build index table with all nodes and the nodes that they can reach
-        print(f"Beams should only have first pair of beams {beams} and root splinter is {root_splinter}")
+        # print(f"Beams should only have first pair of beams {beams} and root splinter is {root_splinter}")
+        print(f"Root splinter is: {root_splinter}")
+        print(f"\nThe Node Lookup Table\n")
+        print(node_index)
         
         
 part_2()
